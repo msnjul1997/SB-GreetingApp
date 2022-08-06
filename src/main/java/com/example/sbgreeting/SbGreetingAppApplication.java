200ad678862dbdbaa333762ea22614dfcb94bd4a
msnkittu859 <Vamshikalyan97>
2022-08-06 14:40:27 +0530

    Added: Greeting Application Development

---
+++ src/main/java/com/example/sbgreeting/SbGreetingAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.sbgreeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbGreetingAppApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SbGreetingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sbgreeting/SbGreetingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbgreeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbGreetingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
